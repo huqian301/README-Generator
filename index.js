d4b63073fd4ad55034f8ec4cd3b93291d0ab755c
huqian301 <huqian301@gmail.com>
2020-09-06 12:26:30 -0700

    README Generator

---
+++ index.js
@@ -0,0 +1,17 @@
+// prompted for their GitHub username
+
+//will be used to make a call to the GitHub API to retrieve their email and profile image
+
+// then be prompted with questions about their project
+// * At least one badge
+// * Project title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+//   * User GitHub profile picture
+//   * User GitHub email
