73830375e459fadae1686e28c46aa241f266ed69
huqian301 <huqian301@gmail.com>
2020-09-12 23:47:51 -0700

    add all the questions to an array

--- index.js
+++ index.js
@@ -33,28 +33,73 @@
 //   * User GitHub email
 
 const fs = require("fs");
-const axios = require("axios");
 const inquirer = require("inquirer");
+const api = require('./utils/api.js')
+const markdown = require('./utils/generateMarkdown.js')
 
-inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
+const questions = [
+  {
+    type: 'input',
+    name:"username",
+    message:"What is your GitHub username?",
+  },
+  {
+    type: 'input',
+    name:"title",
+    message:"What is your project's name?",
+  },
+  {
+    type: 'input',
+    name:"description",
+    message:"Please write a short description of your project.",
+  },
+  {
+    type: 'checkbox',
+    name:"license",
+    message:"What kind of license should your project have?",
+    choices:["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: 'input',
+    name:"installation",
+    message:"What command should be run to install dependencies?",
+    default: "npm i",
+  },
+  {
+    type: 'input',
+    name:"test",
+    message:"What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: 'input',
+    name:"usage",
+    message:"What does the user need to know about using the repo?",
+  },
+  {
+    type: 'input',
+    name:"contribute",
+    message:"What does the user need to know about contributing to the repo?",
+  }
+
+]
+
+
+
 
   //using promises
-  .then(async ({ username }) => {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    try {
-      const { data } = await axios.get(queryUrl);
-      const repoNames = data.map(repo => repo.name).join('\n');
-      
-      fs.writeFile('repos.txt', repoNames, err => {
-        if (err) throw err;
-        console.log('successfully created repos.txt');
-      });
+  // .then(async ({ username }) => {
+  //   const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+  //   try {
+  //     const { data } = await axios.get(queryUrl);
+  //     const repoNames = data.map(repo => repo.name).join('\n');
 
-      } catch (e) {
-        console.log(e);
-      }
-    });
+  //     fs.writeFile('repos.txt', repoNames, err => {
+  //       if (err) throw err;
+  //       console.log('successfully created repos.txt');
+  //     });
+
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   });
