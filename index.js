e6940b7aa950f1a9210cad4e608ed8dcdc1a562f
huqian301 <huqian301@gmail.com>
2020-09-12 21:07:24 -0700

    add git api

--- index.js
+++ index.js
@@ -4,9 +4,25 @@
 
 // then be prompted with questions about their project
 // * At least one badge
+
+// [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://GitHub.com/${username}/)
+
 // * Project title
+
 // * Description
+
+
 // * Table of Contents
+// ## Table of Contents 
+// * [Description](#description)
+// * [View](#view)
+// * [Usage](#usage)
+// * [Project Status](#project-status)
+// * [Built With](#built-with)
+// * [Credits](#credits)
+// * [License](#license)
+
+
 // * Installation
 // * Usage
 // * License
@@ -15,3 +31,30 @@
 // * Questions
 //   * User GitHub profile picture
 //   * User GitHub email
+
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username",
+    name: "username"
+  })
+
+  //using promises
+  .then(async ({ username }) => {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    try {
+      const { data } = await axios.get(queryUrl);
+      const repoNames = data.map(repo => repo.name).join('\n');
+      
+      fs.writeFile('repos.txt', repoNames, err => {
+        if (err) throw err;
+        console.log('successfully created repos.txt');
+      });
+
+      } catch (e) {
+        console.log(e);
+      }
+    });
