5d5491e05c9bd732bc08b17435c65ee5d5cce089
huqian301 <huqian301@gmail.com>
2020-09-16 17:06:22 -0700

    prompt all the questions and writetofile

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,34 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+// * Project title
+
+// * Description
+
+// * Table of Contents
+// ## Table of Contents 
+// * [Description](#description)
+// * [View](#view)
+// * [Usage](#usage)
+// * [Project Status](#project-status)
+// * [Built With](#built-with)
+// * [Credits](#credits)
+// * [License](#license)
+
+
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+//   * User GitHub profile picture
+//   * User GitHub email
+
+
+// * At least one badge
+// [![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](https://GitHub.com/${username}/)
+
 `;
 }
 
